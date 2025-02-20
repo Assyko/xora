5eb08296290aafafdb29ff7692dfa77332651165
Assylzhan <eassyko@gmail.com>
2025-02-20 00:06:54 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Sections/Header.jsx";
+import Hero from "./Sections/Hero.jsx";
+import Features from "./Sections/Features.jsx";
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,39 @@
+import { Element, Link as LinkScroll } from "react-scroll";
+import Button from "../components/Button.jsx";
+import features from "./Features.jsx";
+const Hero = () => {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-3 max-md:pb-32">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+            <div className="capion small-2 uppercase text-p3">
+              Video Editing
+            </div>
+            <h1
+              className="mb-6 h1 text-p4 maxlg:mb-7 max-lg:h2
+            max-md:mb-4 max-md:text-5xl max-md:leading-12"
+            >
+              Amazingly Simple
+            </h1>
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+              We designed Xora Editor to an easy use, quick to learn
+            </p>
+            <LinkScroll to={features} offset={-100} spy smooth>
+              <Button icon="/images/zap.svg">Try it now</Button>
+            </LinkScroll>
+          </div>
+          <div className="absolute -top-32 w-[1230px] left-[calc(50%-340px)] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              className="size-1230 max-lg:h-auto"
+              alt="hero"
+            />
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Hero;
