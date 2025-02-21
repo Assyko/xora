f7732e0dc9150134165ed4622f76a5f7459c64aa
Assylzhan <eassyko@gmail.com>
2025-02-20 21:52:29 -0500

    Pricing buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Sections/Header.jsx";
 import Hero from "./Sections/Hero.jsx";
 import Features from "./Sections/Features.jsx";
+import Pricing from "./Sections/Pricing.jsx";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 };
---
+++ src/Sections/Pricing.jsx
@@ -0,0 +1,71 @@
+import { Element } from "react-scroll";
+import clsx from "clsx";
+import { useState } from "react";
+
+const Pricing = () => {
+  const [monthly, setMonthly] = useState(false);
+  return (
+    <section>
+      <Element name="pricing">
+        <div className="container">
+          <div
+            className="max-w-960 pricing-head_before relative mx-auto
+          border-l border-r border-s2 bg-s1/50 pb-40 pt-28
+            max-xl:max-w-4xl max-lg:border-none max-md:pb-32 maxmd:pt-16"
+          >
+            <h3
+              className="h3 max-lgh4 max-md:h5 z-30relative mx-auto mb-14
+            maxwlg text-center text-p4 max-md:mb-11 max-sm:max-w-sm"
+            >
+              Flexible pricing for teams of all sizes
+            </h3>
+            <div
+              className="relative z-4 mx-auto max-w-md flex w-[375px] rounded-3xl
+            border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]"
+            >
+              <button
+                className={clsx("pricing-head_btn", monthly && "text-p4")}
+                onClick={() => setMonthly(true)}
+              >
+                Monthly
+              </button>
+              <button
+                className={clsx("pricing-head_btn", !monthly && "text-p4")}
+                onClick={() => setMonthly(false)}
+              >
+                Annual
+              </button>
+              <div
+                className={clsx(
+                  "g4 rounded-14 before:h-100 pricing-head_btn_before absolute" +
+                    " left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)] overflow-hidden" +
+                    " shadow-400 transition-transform duration-500",
+                  !monthly && "translate-x-full",
+                )}
+              />
+            </div>
+            <div className="pricing-bg">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute z-2 inset-0 opacity-5 mix-blend-soft-light"
+              />
+            </div>
+          </div>
+          {/*  pricing*/}
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Pricing;
