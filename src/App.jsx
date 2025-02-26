011afe8802be026007e63cbf5c9c9aeb87fc8198
Assylzhan <eassyko@gmail.com>
2025-02-25 20:22:09 -0500

    FaqItem accordion

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Sections/Header.jsx";
 import Hero from "./Sections/Hero.jsx";
 import Features from "./Sections/Features.jsx";
 import Pricing from "./Sections/Pricing.jsx";
+import Faq from "./Sections/Faq.jsx";
 const App = () => {
   return (
     <main className="overflow-hidden">
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
     </main>
   );
 };
